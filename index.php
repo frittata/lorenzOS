124f7034a840895e54c1f4984c2092503b239967
frittata <64194462+frittata@users.noreply.github.com>
2021-01-07 12:55:22 +0100

    2

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <head>
 <link id="css-theme" rel="stylesheet" href="style/dark.css">
 <link rel="stylesheet" href="style/general.css">
+<script src="https://kit.fontawesome.com/bd0bc678fb.js" crossorigin="anonymous"></script>
 <?php include "navbar.html" ?>
 </head>
 <body>
