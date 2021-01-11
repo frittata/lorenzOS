0c19c5945e55ea63e0a534a5ce8a5ea2c3377f50
frittata <64194462+frittata@users.noreply.github.com>
2021-01-11 10:37:52 +0100

    11

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <html>
 <head>
 <link id="css-theme" rel="stylesheet" href="style/dark.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style/general.css">
+<script>
+
+</script>
 <script src="https://kit.fontawesome.com/bd0bc678fb.js" crossorigin="anonymous"></script>
 <?php include "navbar.html" ?>
 </head>
-<body>
+<body onload="myFunction()">
+<div id="loader">
+    <img id="img_load" src="img/windows-8-busy-loader-in-pure-css.gif">
+</div>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,32 +1,14 @@
-function changeTheme(fileCss) {
-    var oldlink = document.getElementsByTagName("link").item(0);
-    var newlink = document.createElement("link");
-    newlink.setAttribute("rel", "stylesheet");
-    newlink.setAttribute("type", "text/css");
-    newlink.setAttribute("href", fileCss);
-    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
 }
 
-function startdisplay() {
-    start = document.getElementById("start-menu");
-    if (start.style.display == "block") { start.style.display = "none" } else { start.style.display = "block" }
+function showPage() {
+    document.getElementById("img_load").style.display = "none";
+    setTimeout(showPage1, 200);
 }
 
-function tabletmode() {
-    var menu = document.getElementById("start-menu");
-    var newmenu = document.getElementById("start-out");
-    menu.id = "start-out";
-    newmenu.id = "start-menu";
-    document.getElementById("start-out").style.display = "none";
-    var desk = document.getElementById("desk");
-    var tab = document.getElementById("tab");
-    if (tab.style.display == "block") {
-        desk.style.display = "block";
-        tab.style.display = "none";
-        document.getElementById("desk-icon").style.display = "inline-flex"
-    } else {
-        desk.style.display = "none";
-        tab.style.display = "block";
-        document.getElementById("desk-icon").style.display = "none"
-    }
+function showPage1() {
+    document.getElementById("loader").style.display = "none";
 }
